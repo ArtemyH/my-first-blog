4db0a10124a283b2907dbbe2eaa4d2ea307ac315
ArtemyH <Prime.95@mail.ru>
2017-02-21 21:30:18 +0000

    Fix settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -110,9 +110,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTHENTICATION_BACKENDS = ['blog.auth_with_email.EmailBackend']
 
-#email must be unique 
-from django.contrib.auth.models import User, models
-User._meta.local_fields[4].__dict__['_unique'] = True
+#email must be unique
+#from django.contrib.auth.models import User, models
+#User._meta.local_fields[4].__dict__['_unique'] = True
 
 
 MEDIA_ROOT = os.path.normpath(os.path.join(BASE_DIR, "../media"))
